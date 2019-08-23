16508c9278ae17d23fbb29c3d7185a4fe092d10f
zzws524 <zzws524@sina.com>
2019-08-23 16:53:51 +0800

    touched the TestFramework.py

--- TestFramework.py
+++ TestFramework.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import d2l
-from mxnet import autograd,nd,gluon
+from mxnet import autograd,nd,gluon,init
 from mxnet.gluon import nn
+from mxnet.gluon import loss as gloss
 import random
 
 def synthetic_data(w,b,num_examples):
@@ -17,17 +18,6 @@ def load_array(data_arrays,batch_size,is_train=True):
     return gluon.data.DataLoader(dataset,batch_size,shuffle=is_train)
 
 
-def linreg(X,w,b):
-    return nd.dot(X,w)+b
-
-def squared_loss(y_hat,y):
-    return (y_hat-y.reshape(y_hat.shape))**2/2
-
-def sgd(params,learning_rate,batch_size):
-    for param in params:
-        param[:]=param-learning_rate*param.grad/batch_size
-
-
 if (__name__=='__main__'):
     true_w=nd.array([2,-3.4])
     true_b=4.2
@@ -38,6 +28,25 @@ if (__name__=='__main__'):
 
     net=nn.Sequential()
     net.add(nn.Dense(1))
+
+    net.initialize(init.Normal(sigma=0.01))
+    loss=gloss.L2Loss() #The squared loss is known as the L2 norm loss
+    trainer=gluon.Trainer(net.collect_params(),'sgd',{'learning_rate':0.03})
+
+    num_epochs=3
+    for epoch in range(1,num_epochs+1):
+        for X,y in data_iter:
+            with autograd.record():
+                l=loss(net(X),y)
+            l.backward()
+            trainer.step(batch_size)
+        l=loss(net(features),labels)
+        print('epoch %d,loss: %f'%(epoch,l.mean().asnumpy()))
+
+    print('After regression, w is ',net[0].weight.data())
+    print('After regression, b is ',net[0].bias.data())
+
+
 
 
 
